c5f014bccfba3a15b1f6ba83f3aa9d9ba3c39b70
ravenn007 <54338975+ravenn007@users.noreply.github.com>
2019-08-20 22:18:08 -0300

    adicionei arquivos upload superMaquina

---
+++ supermaquina/SUPERMAQUINA.ino
@@ -0,0 +1,16 @@
+/**
+   Sequencial de LEDs (efeito supermáquina)
+   @author - Ravena Angelo
+*/
+
+
+void setup() {
+  pinMode (13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (13, HIGH);
+
+
+}
